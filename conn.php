9a562b911a29de23ae2598e1e1e2d8ddf257a071
Gideon <twumgideonasare@gmail.com>
2024-04-23 14:58:25 -0700

    adding a background image

--- conn.php
+++ conn.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "restaurant";
+$dbname = "chopbar_db";
 
 //Creating a connection
 $conn = new mysqli($servername, $username , $password , $dbname );
